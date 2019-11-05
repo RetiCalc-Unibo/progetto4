e6cafddaee6bed815e2173ecb9a3e5a7ff1341eb
Hiari <h.pizzinicavagna@gmail.com>
2019-11-05 18:56:25 +0000

    Update Server.c

--- Server.c
+++ Server.c
@@ -37,7 +37,6 @@ int main(int argc, char * argv[]){
 				exit(2);
 			}
 		}
-
 		port = atoi(argc[1]);
 		if(port < 1024 || port> 65535){
 				printf("Server: Ports must be between 1024-65535.\n");
@@ -48,49 +47,41 @@ int main(int argc, char * argv[]){
 		exit(4);
 	}
 
-
 	//Inizializzo indirizzo server
-
 	memset((char *)&serveraddr, 0,sizeof(serveraddr));
 	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
 	serveraddr.sin_port = htons(port);
 
 
 	//Creazione socket TCP listen
-
 	if ((listedfd = socket(AF_INET, SOCK_STEAM, 0)) < 0){
 		perror("Apertura socket TCP"); 
 		exit(5);
 	}
 
 	//Set option socket TCP
-
 	//SO_REUSE --> reuse address; option_value --> on = 1
 	if(setsockopt(listenfd, SOL_SOCKET, SO_REUSE, &on, sizeof(on)) < 0){
 		perror("Set opzioni socker TCP");
 		exit(6);
 	}
-
 	if(bind(listenfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0){
 		perror("Bind socket TCP");
 		exit(7);
 	}
 
 	//Creazione socket UDP 
-
 	if ((udpfd = socket (AF_INET, SOCK_DGRAM, 0)) < 0){
 		perror("Apertura socket UDP");
 		exit(8);
 	}
 
 	//Set option socket UDP
-
 	//SO_REUSE --> reuse address; option_value --> on = 1
 	if(setsockopt(udpfd, SOL_SOCKET, SO_REUSE, &on, sizeof(on)) < 0){
 		perror("Set opzioni socker UDP");
 		exit(9);
 	}
-
 	if(bind(udpfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0){
 		perror("Bind socket UDP");
 		exit(10);
@@ -100,9 +91,7 @@ int main(int argc, char * argv[]){
 	// NB! --> EINTR corrisponde a interruzione da richieste
 	signal(SIGCHLD, handler);
 	FD_ZERO($rset); //set FD mask
-
 	maxfdpl = max(listenfd, udpfd) + 1; //determino fd più alto
-
 	for(;;){
 		//Preparazione maschera --> writefds, exceptfds e timeout settati null
 		// 							si considera solo readfds
@@ -116,7 +105,6 @@ int main(int argc, char * argv[]){
 				exit(11);
 			}
 		}
-
 		//Richieste UDP in sequenziale
 		if(FD_ISSET(udpfd, &rset)){
 			len = sizeof(struct sockaddr_in);
